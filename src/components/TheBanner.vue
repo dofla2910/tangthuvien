3dd12a29edeb9c4be4de783770c4daf51e29fb10
Dương Chí Hiếu <nineoxon2005@gmail.com>
2024-09-13 02:21:40 +0700

    feat: add web banner

---
+++ src/components/TheBanner.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .banner
+    a(href='/tai-app')
+      img(src='/src/assets/images/banner.jpg', alt='')
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  .banner {
+    max-width: 100%;
+    margin: 20px 0;
+
+    img {
+      max-width: 100%;
+      object-fit: cover;
+    }
+  }
+</style>
